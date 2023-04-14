12d82cfea516637bf3987c1ca221509eb6ebf9bb
MarceloZych <74663103+MarceloZych@users.noreply.github.com>
2023-04-14 13:04:01 -0300

    Day 2: Operators

--- Day1.ts
+++ Day1.ts
@@ -1,35 +1,35 @@
-'use strict';
+// 'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-let inputString: string = '';
-let inputLines: string[] = [];
-let currentLine: number = 0;
-process.stdin.on('data', function (inputStdin: string): void {
-    inputString += inputStdin;
-});
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+// let inputString: string = '';
+// let inputLines: string[] = [];
+// let currentLine: number = 0;
+// process.stdin.on('data', function (inputStdin: string): void {
+//     inputString += inputStdin;
+// });
 
-process.stdin.on('end', function (): void {
-    inputLines = inputString.split('\n');
-    inputString = '';
-    main();
-});
+// process.stdin.on('end', function (): void {
+//     inputLines = inputString.split('\n');
+//     inputString = '';
+//     main();
+// });
 
-function readLine(): string {
-    return inputLines[currentLine++];
-}
+// function readLine(): string {
+//     return inputLines[currentLine++];
+// }
 
-function main() {
-    // Enter your code here
-    const i = 4;
-    const d = 4.0;
-    const s = 'HackerRank'
+// function main() {
+//     // Enter your code here
+//     const i = 4;
+//     const d = 4.0;
+//     const s = 'HackerRank'
 
-    const i1 = parseInt(readLine());
-    const d1 = parseFloat(readLine());
-    const s1 = readLine();
+//     const i1 = parseInt(readLine());
+//     const d1 = parseFloat(readLine());
+//     const s1 = readLine();
 
-    console.log(i1 + i)
-    console.log((d1 + d).toFixed(1))
-    console.log(s+ ' ' + s1);
-}
+//     console.log(i1 + i)
+//     console.log((d1 + d).toFixed(1))
+//     console.log(s+ ' ' + s1);
+// }
